27582b98305a68f11fb518e460fb1c3c5e4dd308
surae-noether <surae.noether@gmail.com>
2015-05-17 21:20:45 -0400

    Create simulator.py
    Runs the whole deal.

---
+++ simulator.py
@@ -0,0 +1,9 @@
+from blockChain import *
+from simulation import *
+from hashRate import *
+
+def letsGetThisPartyStarted(params=[]):
+    s = simulation([])
+    s.runSim()
+
+letsGetThisPartyStarted()
