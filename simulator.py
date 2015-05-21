2f331cac2cc360d51fd9a0c605bd021d3f99fa53
surae-noether <surae.noether@gmail.com>
2015-05-21 15:30:22 -0400

    Update simulator.py
    Run this (after compiling everything else) to execute a simulation.
    
    Change constants in Constants.py, like MAX_RUN_TIME, in order to tweak how the simulation goes.
    
    The user has some control over thisHashRate, which is the input hash rate that produces the block chain, through this file, although if they call letsGetThisPartyStarted without any parameters, all defaults will be used throughout (and the default hash rate is flat at 1.0 H/s).

--- simulator.py
+++ simulator.py
@@ -1,9 +1,33 @@
+from Constants import *
+from hashRate import *
 from blockChain import *
 from simulation import *
-from hashRate import *
 
-def letsGetThisPartyStarted(params=[]):
-    s = simulation([])
+import random
+import operator as op
+import functools
+import time
+import os
+import csv
+import numpy
+
+import sys
+import math
+
+def letsGetThisPartyStarted(params=None):
+    if params==None:
+        #print("No parameters passed when party getting started. Using defaults.")
+        s = simulation([])
+    else:
+        #print("Parameters passed in to get a party started! Indeed, params = " + str(params))
+        s = simulation(params)
     s.runSim()
 
-letsGetThisPartyStarted()
+
+fifths = MAX_RUN_TIME*0.2
+thisTime = [0.0, 1.0*fifths, 2.0*fifths, 3.0*fifths, 4.0*fifths, 5.0*fifths]
+theseValues = [10.0, 20.0, 40.0, 80.0, 100.0]
+thisHashRate = hashRate([thisTime,theseValues,DESC_EXAMPLE,MAX_RUN_TIME])
+
+params = [MAX_RUN_TIME, LAMBDA_TARGET, DIFFICULTY_FORMULA, STARTING_DIFFICULTY, DIFF_ADJ_PERIOD, DIFF_SAMPLE_SIZE, TIME_SAMPLE_SIZE, thisHashRate]
+letsGetThisPartyStarted(params) 
