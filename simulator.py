b8ef5117aac8def6289a6b796c3682307b525a6f
surae-noether <surae.noether@gmail.com>
2015-05-25 15:31:33 -0400

    Update simulator.py to check directory existence
    Adds make_sure_path_exists to check if a directory exists and, if not, creates it. Can be used when writing data to file to prevent errors.

--- simulator.py
+++ simulator.py
@@ -10,9 +10,9 @@ import time
 import os
 import csv
 import numpy
-
 import sys
 import math
+import errno
 
 def letsGetThisPartyStarted(params=None):
     if params==None:
@@ -23,6 +23,24 @@ def letsGetThisPartyStarted(params=None):
         s = simulation(params)
     s.runSim()
 
+###########################################################
+
+def printToLog(self, text):
+    self.logFile = open(self.logFileName, "a")
+    if(not self.logFile):
+        print("Error in (simulator __printToLog__): Log file is an empty object!")
+    else:
+        self.logFile.write(text + "\n")
+    self.logFile.close()
+
+def make_sure_path_exists(self, path):
+    try:
+        os.makedirs(path)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
+    
+###########################################################
 
 fifths = MAX_RUN_TIME*0.2
 thisTime = [0.0, 1.0*fifths, 2.0*fifths, 3.0*fifths, 4.0*fifths, 5.0*fifths]
